9c39020834a24007488ea432dab551a172ef7682
leedoing <lluckyy77@gmail.com>
2019-06-11 17:56:48 +0900

    언제 만드노. 노? 신고합니다

--- src/assets/js/addUrl.js
+++ src/assets/js/addUrl.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 const addBtn = document.getElementById("jsAddUrl");
+const closeBtn = document.getElementById("jsClose");
 
 const clickAddUrl = event => {
   event.preventDefault();
-  console.log("ok");
+  document.querySelector(".bg-modal").style.display = "flex";
+};
+
+const clickCloseAddUrl = event => {
+  event.preventDefault();
+  document.querySelector(".bg-modal").style.display = "none";
 };
 
 async function init() {
   addBtn.addEventListener("click", clickAddUrl);
+  closeBtn.addEventListener("click", clickCloseAddUrl);
 }
 
 if (addBtn) {

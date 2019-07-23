a3d89b13a8cdd42d26aad24ec42e54c3af57bc97
leedoing <lluckyy77@gmail.com>
2019-07-23 15:02:51 +0900

    mail

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 	PATH="$PATH:/root/.nvm/versions/node/v10.16.0/bin"
 	pm2 delete url-checker
 	rm -rf /root/url_checker
-	git clone https://github.com/leedoing/url_checker.git /root/url_checker
+	git clone https://leedoing:tjdrhd1324@github.com/leedoing/url_checker.git /root/url_checker
 	cd /root/url_checker/
 	yarn install
 	yarn run start
-	mv /root/.env /root/url_checker/.env
+	cp /root/.env /root/url_checker/.env
